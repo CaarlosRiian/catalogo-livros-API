115ca701f03f5de8f8f7f41f1280f582e1d4f4ec
Rian <rlina1785@gmail.com>
2024-03-14 09:59:05 -0300

    pequena alteracao

--- frontend/catalogo/src/Editar.js
+++ frontend/catalogo/src/Editar.js
@@ -1,50 +1,50 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 
 function Editar() {
     const { id } = useParams();
-    const [livro, setLivro] = useState([]);
-    const [titulo, setTitulo] = useState([]);
-    const [autor, setAutor] = useState([]);
-    const [anoPublicado, setAnoPublicado] = useState([]);
+    const [titulo, setTitulo] = useState('');
+    const [autor, setAutor] = useState('');
+    const [anoPublicado, setAnoPublicado] = useState('');
     const [autores, setAutores] = useState([]);
-    // GET - Livro
-    useEffect(() => {
-        fetch(`http://localhost:8000/livros/${id}/`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((resp) => resp.json())
-            .then((data) => {
-                setLivro(data);
-                console.log("consegui", data);
-            })
-            .catch((err) => console.log(err));
-    }, []);
-    
-    // GET - Autores
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:8000/autores/');
-        const data = await response.json();
-        setAutores(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
 
-    fetchData();
-  }, []);
+    // GET - Livro
+useEffect(() => {
+    fetch(`http://localhost:8000/livros/${id}/`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setTitulo(data.titulo);
+            setAutor(data.autor || ''); 
+            setAnoPublicado(data.ano_publicado);
+            console.log("consegui", data);
+        })
+        .catch((err) => console.log(err));
+}, [id]);
+
+    // GET - Autores
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/autores/');
+                const data = await response.json();
+                setAutores(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     // Update 
-    const handleSubmit = (e) => {
-        console.log('oi!');
+    const handleSubmit = useCallback((e) => {
         e.preventDefault();
         const livro = {
             titulo: titulo,
@@ -66,14 +66,14 @@ function Editar() {
                 return response.json();
             })
             .then((data) => {
-                console.log("Dados atualizados com sucesso:", data);
+                alert("Dados atualizados com sucesso:", data);
             })
             .catch((error) => {
                 console.error("Erro:", error);
             });
 
         console.log(livro);
-    };
+    }, [id, titulo, autor, anoPublicado]);
 
     return (
         <div className="App">
@@ -81,22 +81,22 @@ function Editar() {
             <h1>Editando Livro</h1>
             <form onSubmit={handleSubmit}>
                 <label>Título:</label>
-                <input type="text" onChange={(e) => setTitulo(e.target.value)}/>
-                <label>Ator:</label>
+                <input type="text" value={titulo || ''} onChange={(e) => setTitulo(e.target.value)}/>
+                <label>Autor:</label>
                 <select 
-                onClick={(e) => console.log(e.target.value)}
-                onChange={(e) => setAutor(e.target.value)}
+                    onClick={(e) => console.log(e.target.value)}
+                    onChange={(e) => setAutor(e.target.value)}
+                    value={autor || ''}
                 >
-                    <option value="">Selecionar um ator</option>
+                    <option value="">Selecionar um autor</option>
                     {autores.map((aut) => (
                         <option key={aut.id} value={aut.id}>
                             {aut.nome}
                         </option>
                     ))}
                 </select>
-                <label >Ano publicado:</label>
-                <input type="date"  
-                onChange={(e) => setAnoPublicado(e.target.value)}/>
+                <label>Ano publicado:</label>
+                <input type="date" value={anoPublicado || ''} onChange={(e) => setAnoPublicado(e.target.value)}/>
                 <input className="botao-editar-2" type="submit" value="Editar" onClick={handleSubmit}/>
             </form>
         </div>
